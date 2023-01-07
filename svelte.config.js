6fb5ac8eef36e882e30846fb5aa8ff56a045e528
Kush Patel <kush@kush.in>
2023-01-07 04:43:33 +0000

    azure adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import azure from "svelte-adapter-azure-swa";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-	preprocess: [
-		preprocess({
-			postcss: true,
-		}),
-	],
-	vitePlugin: {
-		experimental: {
-			inspector: {
-				holdMode: true,
-			}
-		}
-	}		
-
+  kit: {
+    adapter: azure(),
+  },
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+  vitePlugin: {
+    experimental: {
+      inspector: {
+        holdMode: true,
+      },
+    },
+  },
 };
 
 export default config;
